421f0eaa41c84ccb442d0e02377c7a356f818d53
turupawn <ahmed.hn.43@gmail.com>
2021-06-14 16:24:54 -0600

    producers contract

---
+++ contracts/Producers.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.5;
+
+contract Producers
+{
+  struct Producer {
+    uint256 id;
+    string name;
+    string email;
+    string phone;
+    string whatsapp;
+    string region;
+    string country;
+    string bio;
+    string image;
+  }
+  struct Farm {
+    string name;
+    string country;
+    string region; // ??
+    uint256 lat;   // ??
+    uint256 lang;  // ??
+    string description;
+  }
+  struct Lot {
+    uint256 height;
+    string variety;
+    string procedure;
+    uint256 size;
+  }
+  
+  uint256 producer_count;
+  mapping(uint256 => Producer) public producers;
+  
+  function addProducer(
+    string memory name,
+    string memory email,
+    string memory phone,
+    string memory whatsapp,
+    string memory region,
+    string memory country,
+    string memory bio,
+    string memory image
+    ) public
+  {
+    producer_count += 1;
+    producers[producer_count] = Producer(producer_count,
+      name,
+      email,
+      phone,
+      whatsapp,
+      region,
+      country,bio,
+      image);
+  }
+}
