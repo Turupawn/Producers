e5a320627a1ec28a1950f0de37e419723b6a234a
turupawn <ahmed.hn.43@gmail.com>
2021-06-14 17:26:07 -0600

    fix 5

--- test/Producers.test.js
+++ test/Producers.test.js
@@ -21,5 +21,12 @@ contract('Producers', function () {
 
     var first_producer = await this.producers.producers(1)
     expect(first_producer.name).to.equal('name');
+    expect(first_producer.email).to.equal('email');
+    expect(first_producer.phone).to.equal('phone');
+    expect(first_producer.whatsapp).to.equal('whatsapp');
+    expect(first_producer.region).to.equal('region');
+    expect(first_producer.country).to.equal('country');
+    expect(first_producer.bio).to.equal('bio');
+    expect(first_producer.image).to.equal('image');
   });
 });
