8eb56973f202240f73cee9a5a0c5e9d68a4b2de7
turupawn <ahmed.hn.43@gmail.com>
2021-06-30 18:14:29 -0600

    added events, admins and edits

--- contracts/Producers.sol
+++ contracts/Producers.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.5;
 
 contract Producers
 {
+  /* Structs */
   struct Producer {
     string name;
     string email;
@@ -15,9 +16,67 @@ contract Producers
     string image;
     address editor;
   }
+
+  /* Events */
+  event AddProducerEvent(
+    address sender,
+    uint256 id,
+    string name,
+    string email,
+    string phone,
+    string whatsapp,
+    string region,
+    string country,
+    string bio,
+    string image,
+    address editor
+  );
+
+  event EditProducerEvent(
+    address sender,
+    uint256 id,
+    string name,
+    string email,
+    string phone,
+    string whatsapp,
+    string region,
+    string country,
+    string bio,
+    string image,
+    address editor
+  );
   
+  /* Public Variables */
   uint256 public producer_count;
   mapping(uint256 => Producer) public producers;
+  mapping(address => bool) public addessIsAdmin;
+
+  constructor()
+  {
+    addessIsAdmin[msg.sender] = true;
+  }
+
+  /* Modifer */
+  modifier addressIsAdmin(address _address) {
+    require(addessIsAdmin[_address], "Sender must be admin");
+    _;
+  }
+
+  modifier addressIsEditor(address editor) {
+    require(msg.sender == editor, "Sender must be editor.");
+    _;
+  }
+
+  /* Public Functions */
+  function addAdminAddress(address _address) public addressIsAdmin(msg.sender)
+  {
+    addessIsAdmin[_address] = true;
+  }
+
+  function removeAdminAddress(address _address) public addressIsAdmin(msg.sender)
+  {
+    addessIsAdmin[_address] = false;
+  }
   
   function addProducer(
     string memory name,
@@ -29,18 +88,34 @@ contract Producers
     string memory bio,
     string memory image,
     address editor
-    ) public
+    ) public addressIsAdmin(msg.sender)
   {
     producers[producer_count] = Producer(
-      name,
-      email,
-      phone,
-      whatsapp,
-      region,
-      country,
-      bio,
-      image,
-      editor);
+      name, email, phone, whatsapp, region, country, bio, image, editor
+    );
+    emit AddProducerEvent(
+      msg.sender, producer_count, name, email, phone, whatsapp, region, country, bio, image, editor
+    );
     producer_count += 1;
+  }
+
+  function editProducer(
+    uint256 id,
+    string memory name,
+    string memory email,
+    string memory phone,
+    string memory whatsapp,
+    string memory region,
+    string memory country,
+    string memory bio,
+    string memory image,
+    address editor) public addressIsEditor(editor) // addressIsAdmin(msg.sender) can admin edit producers?
+  {
+    producers[id] = Producer(
+      name, email, phone, whatsapp, region, country, bio, image, editor
+    );
+    emit EditProducerEvent(
+      msg.sender, id, name, email, phone, whatsapp, region, country, bio, image, editor
+    );
   }
 }
