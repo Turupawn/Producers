f2c9f27f19ea867cc4479a2383f77d0f4fc6560a
turupawn <ahmed.hn.43@gmail.com>
2021-08-21 12:49:15 -0600

    stuffx

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ var accounts
 var web3
 
 const getProducer = async () => {
-  var producer_id = document.getElementById("producer_id").value
+  var producer_id = document.getElementById("get_producer_id").value
   producer_json = await contract.methods
     .producers(producer_id).call()
   document.getElementById("producer").innerHTML = JSON.stringify(producer_json)
